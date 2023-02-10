36175edb647361f394fc027359870f0ce7d3cf9a
Samuel <samuelpl0888@gmail.com>
2023-02-10 00:02:16 -0400

    Add skip and limit methods

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('-name price');
+  const products = await Product.find({}).sort('name').select('name price');
   res.status(200).json({
     status: 'success',
     data: products,
@@ -42,6 +42,16 @@ const getAllProducts = async (req, res) => {
     const selectList = select.split(',').join(' ');
     result = result.select(selectList);
   }
+
+  // PAGINATION
+
+  // page query passed by user or 1 by default
+  const page = Number(req.query.page) || 1;
+  // limit query passed by user or 10 by default
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
 
   // RESPONSE
   const products = await result;
